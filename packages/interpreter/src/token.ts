5b6e6d652202e82f7809d35d0d327d6ebaa891e7
Lauren Tan <laurent@netflix.com>
2019-12-08 00:04:10 -0800

    Implement basic interpreter

---
+++ packages/interpreter/src/token.ts
@@ -0,0 +1,53 @@
+import hasOwnProperty from './utils/has-own-property';
+
+export const enum TokenType {
+  ILLEGAL = 'ILLEGAL',
+  EOF = 'EOF',
+
+  // Identifiers + Literals
+  IDENT = 'IDENT',
+  INT = 'INT',
+
+  // Operators
+  ASSIGN = '=',
+  PLUS = '+',
+
+  // Delimiters
+  COMMA = ',',
+  SEMICOLON = ';',
+
+  LPAREN = '(',
+  RPAREN = ')',
+  LBRACE = '{',
+  RBRACE = '}',
+
+  // Keywords
+  FUNCTION = 'FUNCTION',
+  LET = 'LET',
+}
+
+export type LiteralType = string | 0;
+
+export type Token = {
+  type: TokenType;
+  literal: LiteralType;
+};
+
+const KEYWORDS = {
+  fn: TokenType.FUNCTION,
+  let: TokenType.LET,
+};
+
+export default function createToken(
+  type: TokenType,
+  literal: LiteralType
+): Token {
+  return { type, literal };
+}
+
+export function lookupIdentifier(ident: string) {
+  if (hasOwnProperty(KEYWORDS, ident)) {
+    return KEYWORDS[ident];
+  }
+  return TokenType.IDENT;
+}
